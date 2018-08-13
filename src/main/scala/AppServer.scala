eb47a17cf91c31798da261bd84c604574131976e
Aleksandra <littlejellycat@yandex.ru>
2018-08-13 23:42:06 +0300

    Yet another minor refactoring

--- src/main/scala/AppServer.scala
+++ src/main/scala/AppServer.scala
@@ -1,4 +1,7 @@
+import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.{HttpApp, Route}
+
+import scala.util.{Failure, Success}
 
 object AppServer extends HttpApp with App {
   override def routes: Route =
@@ -7,7 +10,10 @@ object AppServer extends HttpApp with App {
         parameters('id.as[Int].*) { ids =>
           ids.toList match {
             case Nil => complete("{}")
-            case _ => complete(StatsService.getStats(ids.toList))
+            case idList => onComplete(StatsService.getStats(idList)) {
+              case Success(result) => complete(result)
+              case Failure(exception) => complete(StatusCodes.InternalServerError -> exception.getMessage)
+            }
           }
         }
       }
